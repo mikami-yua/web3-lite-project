176d47ffe0c8577c3079e90822499c3372d3cf35
mikami-yua <lovejiaxi_1@163.com>
2025-01-22 23:50:37 +0800

    section 5 part-A

--- contracts/FunctionCall/callee.sol
+++ contracts/FunctionCall/callee.sol
@@ -3,9 +3,14 @@ pragma solidity  >=0.8.2 <0.9.0;
 
 import "contracts/FunctionCall/interfaceCallee.sol";
 
-contract Callee is ICallee{ // 合约实现接口合约，需要实现接口中的所有接口函数
+contract Callee{ // 合约实现接口合约，需要实现接口中的所有接口函数
     uint public  x;
-    function setX(uint _x) public {
+    function setX(uint _x) public returns(uint) {
         x=_x;
+        return x;
+    }
+
+    fallback() external { 
+        x=100000;
     }
 }
--- contracts/FunctionCall/caller.sol
+++ contracts/FunctionCall/caller.sol
@@ -1,15 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity  >=0.8.2 <0.9.0;
 
-import "contracts/FunctionCall/callee.sol"; // import 导入被调用者的定义
+//import "contracts/FunctionCall/callee.sol"; // import 导入被调用者的定义
+// 动态调用不需要import
 
 contract Caller{
     address calleeAddress;
+    uint public xx;
     constructor (address _calleeAddress){
         calleeAddress=_calleeAddress;
     }
-    function setCalleeX(uint _x) public {
-        Callee callee=Callee(calleeAddress);// 地址类型强制转换为合约类型
-        callee.setX(_x);
+    // function setCalleeX(uint _x) public {
+    //     Callee callee=Callee(calleeAddress);// 地址类型强制转换为合约类型
+    //     callee.setX(_x);
+    // }
+
+    function setCalleeDynamic(uint _x)public {
+        // 动态调用首先获取calldata
+        bytes memory cd=abi.encodeWithSignature("setY(uint256)", _x);// 需要使用全程，反射不能用别名
+        (bool suc,bytes memory rst)=calleeAddress.call(cd);
+        if (!suc){
+            revert("call failed");
+        }
+        // (uint x)=abi.decode(rst, (uint));
+        // xx=x;
     }
 }
